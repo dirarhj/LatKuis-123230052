4b4cf3552c81b99e13ab34a57a62228f241a0f16
Dira <hasikotopo123@gmail.com>
2026-03-11 20:25:19 +0700

    aku udah selesai

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,19 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_kuis_a/screen/moviel_list_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController userC = TextEditingController();
+  final TextEditingController passC = TextEditingController();
+  bool isLoggedIn = false;
+
+  void _login(){
+    if (userC.text == 'Dira' && passC.text == '052') {
+      setState(() {
+        isLoggedIn = true;
+      });
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MovieListPage(username: userC.text,)));
+    } else {
+      setState(() {
+        isLoggedIn = false;
+      });
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(isLoggedIn ? 'Login Berhasil' : 'Gagal'), 
+      backgroundColor: isLoggedIn ? Colors.green : Colors.red,
+      ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    /*
-      buatlah tampilan login sederhana dengan menggunakan widget TextField untuk input username dan password, serta ElevatedButton untuk tombol login.
-      - TextField untuk username dengan hint text "Username"
-      - TextField untuk password dengan hint text "Password" dan obscureText true
-      - ElevatedButton dengan text "Login" yang ketika ditekan akan menampilkan snackbar dengan pesan
-        "Login berhasil" jika username dan password tidak kosong, atau "Login gagal" jika salah satu atau kedua field kosong.
-      - jika login berhasil, navigasikan ke halaman MovieListPage (buat halaman MovieListPage 
-    
-     */
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Login', 
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text("Welcome back to MiniLons!",
+            style: TextStyle(
+              fontSize: 15
+            ),
+            ),
+            SizedBox(height: 20),
+            _usernameField(),
+            SizedBox(height: 20),
+            _passwordField(),
+            SizedBox(height: 20),
+            //button buat login
+            ElevatedButton(
+              //fungsinya ini boss
+              onPressed: _login, 
+              //styling button kayak gini gaess
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.white,
+                minimumSize: Size(double.infinity, 45)
+              ),
+              child: Text('Login')
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //custom widget buat user field
+  Widget _usernameField() {
+    return TextField(
+            //nambahin controller
+            controller: userC,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'username.....',
+            ),
+    );
+  }
+
+  //custom widget buat pass field
+  Widget _passwordField() {
+    return TextField(
+            //nambahin controler
+            controller: passC,
+            //ini buat nge hide karakter kayak password
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'password.....',
+            ),
+    );
   }
 }
--- lib/screen/moviel_list_page.dart
+++ lib/screen/moviel_list_page.dart
@@ -1,18 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_kuis_a/models/movie_model.dart';
+import 'package:latihan_kuis_a/screen/movie_detail_page.dart';
 
-class MovieListPage extends StatelessWidget {
-  const MovieListPage({super.key});
+class MovieListPage extends StatefulWidget {
+  final String username;
+  const MovieListPage({super.key, required this.username});
 
   @override
+  State<MovieListPage> createState() => _MovieListPageState();
+}
+
+class _MovieListPageState extends State<MovieListPage> {
+  @override
+  List<bool> savedMovies = List.generate(movieList.length, (index) => false);
+  final List<MovieModel> movies = movieList;
   Widget build(BuildContext context) {
-    /*
-      implementasikan widget untuk menampilkan list movie yang sudah diambil dari dummy data
-        - gunakan ListView.builder untuk menampilkan list movie
-        - setiap item movie ditampilkan menggunakan Card widget yang berisi judul movie dan poster movie
-        - ketika item movie ditekan, navigasikan ke halaman MovieDetailPage dengan membawa data movie yang dipilih
-        
-    
-     */
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome, ${widget.username}!",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 15),
+            Expanded(
+              child: ListView.builder(
+                itemCount: movies.length,
+                itemBuilder: (context, index) {
+                  final movie = movies[index];
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Image.network(
+                        movie.imgUrl,
+                        width: 50,
+                        height: 70,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(
+                        "${movie.title} (${movie.year})",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(movie.genre),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  color: Colors.orange,
+                                  size: 16,
+                                ),
+                                const SizedBox(width: 5),
+                                Text("${movie.rating}/10"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          savedMovies[index]
+                              ? Icons.bookmark
+                              : Icons.bookmark_border,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            savedMovies[index] = !savedMovies[index];
+                          });
+                        },
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MovieDetailPage(movie: movie),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
